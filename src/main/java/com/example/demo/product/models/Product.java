08668ec5f028f087cf11c7d1cf3376f7c10dba3a
Acer <simone200354@gmail.com>
2023-06-23 13:11:38 +0200

    init

---
+++ src/main/java/com/example/demo/product/models/Product.java
@@ -0,0 +1,16 @@
+package com.example.demo.product.models;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+
+@Getter
+@AllArgsConstructor
+public class Product {
+    String name;
+    String description;
+    double price;
+
+
+}
---
+++ src/main/java/com/example/demo/product/presentation/ProductController.java
@@ -0,0 +1,22 @@
+package com.example.demo.product.presentation;
+
+import com.example.demo.product.models.Product;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class ProductController {
+
+
+    @GetMapping("/products")
+    public List<Product> getProducts(){
+        return List.of(
+                new Product( "Product 1","Description 2",100),
+                new Product("Product 2", "Description 2",111),
+                new Product("Product 3","Description 2",222)
+        );
+    }
+
+}
