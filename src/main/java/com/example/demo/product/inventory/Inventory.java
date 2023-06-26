6faa09e98be0b05acf81293518517ee181b9179d
Acer <simone200354@gmail.com>
2023-06-26 10:26:50 +0200

    init

---
+++ src/main/java/com/example/demo/product/inventory/Inventory.java
@@ -0,0 +1,4 @@
+package com.example.demo.product.inventory;
+
+public class Inventory {
+}
--- src/main/java/com/example/demo/product/models/Product.java
+++ src/main/java/com/example/demo/product/models/Product.java
@@ -1,16 +1,34 @@
 package com.example.demo.product.models;
 
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Transient;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
 
 
 @Getter
+@Setter
+@NoArgsConstructor
 @AllArgsConstructor
+@Entity
 public class Product {
-    String name;
-    String description;
-    double price;
+    @Id
+    @GeneratedValue
+    private long id;
+    private String name;
+    private String description;
+    private double price;
+    private String category;
+    private LocalDate expiryDate;
+    @Transient
+    private int daysToExpire;
 
 
 }
